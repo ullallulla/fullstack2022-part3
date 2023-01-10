e345499b20384b19fca23c3407057fb1ba7cfedb
Aleksi Ojansivu <aleksi.ojansivu@gmail.com>
2023-01-10 23:11:52 +0200

    ex 3.8

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+
+morgan.token('body', (request, response) => JSON.stringify(request.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     {
@@ -25,8 +29,6 @@ let persons = [
         number: "39-23-6423122"
     }
 ]
-
-app.use(express.json())
 
 app.get('/info', (request, response) => {
 
