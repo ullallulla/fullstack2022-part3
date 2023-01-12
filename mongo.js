0b0ee176dcb4b52adeb0c0827cdd8a51cec95aed
Aleksi Ojansivu <aleksi.ojansivu@gmail.com>
2023-01-12 18:26:14 +0200

    ex 3.12

---
+++ mongo.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('Please provide the password as an argument: node mongo.js <password>')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url = `mongodb+srv://fullstack:${password}@phonebook.5r8pz.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+mongoose
+    .connect(url)
+    .then((result) => {
+        console.log('connected')
+
+        if (name && number) {
+            const person = new Person({
+                name,
+                number
+            })
+
+            return person.save().then((result) => {
+                console.log(`added ${name} number ${number} to phonebook`)
+                return mongoose.connection.close()
+            })
+        }
+        else {
+            console.log('phonebook:')
+            Person.find({}).then(result => {
+                result.forEach(person => {
+                    console.log(`${person.name} ${person.number}`)
+                })
+                mongoose.connection.close()
+            })
+        }
+    })
+    .catch((err) => console.log(err))
